439df8a86ec3e67170abab4fdb2d63d622246c02
Turki <TurkiMA1445@outlook.com>
2025-04-01 09:48:39 +0300

    git commit message -m "added main functions"

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
 import javax.swing.JFrame;
+import java.awt.*;
+import javax.swing.JPanel;
 
 public class Main extends JFrame{
     
-    public static void Main(String args[])
+    public Main(String title )
     {
+        super(title);
 
+        JPanel panel = (JPanel)this.getContentPane();
+
+        panel.setLayout(new BorderLayout());
+        this.pack();
+        setVisible(true);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void main(String args[])
+    {
+        Main frame = new Main("title");
     }
 }
