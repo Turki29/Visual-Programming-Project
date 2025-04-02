f86b723bf1bf16e55b96b256a9172b1d804984e0
Turki <TurkiMA1445@outlook.com>
2025-04-02 17:57:04 +0300

    تصميم واجهة تسجيل الدخول

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 import java.awt.*;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Main extends JFrame{
     
+    JPanel panel1;
+    JTextField fieldUsername, fieldPassword; 
+    JLabel lblHeading, lblUsername, lblPassword ;
+    JButton btnRegister, btnLogin;
     public Main(String title )
     {
         super(title);
@@ -11,6 +19,34 @@ public class Main extends JFrame{
         JPanel panel = (JPanel)this.getContentPane();
 
         panel.setLayout(new BorderLayout());
+
+        fieldUsername = new JTextField(10);
+        fieldPassword = new JTextField(10);
+
+        lblHeading = new JLabel("Login ");
+        lblUsername = new JLabel("Username :");
+        lblPassword = new JLabel("Password :");
+        
+        btnLogin = new JButton("Login");
+        btnLogin.addActionListener(new UserInterface);
+        btnRegister = new JButton("Register");
+
+        panel1 = new JPanel(new GridLayout(3,2));
+
+        panel1.add(lblUsername);
+        panel1.add(fieldUsername);
+
+        panel1.add(lblPassword);
+        panel1.add(fieldPassword);
+
+        panel1.add(btnLogin);
+        panel1.add(btnRegister);
+
+        panel.add(lblHeading,  BorderLayout.NORTH);
+        panel.add(panel1); 
+        
+
+        
         this.pack();
         setVisible(true);
 
