4bade47d5fa7b0f4fca8de786d186d6bc6730225
Nawaf <nawafmajed999@gmail.com>
2025-04-14 15:00:35 +0300

    Add files via upload

--- DoctorInterface.java
+++ DoctorInterface.java
@@ -1,86 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.doctorinterface;
 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 public class DoctorInterface extends JFrame {
-private JButton Can1Btn, ReschdBtn,AccessBtn,CnfrmBtn,Can2Btn,LogOutBtn;
+private JButton Can1Btn, ReschdBtn,AccessBtn,AddMedBtn,CnfrmBtn,Can2Btn,LogOutBtn;
 private JComboBox Apptmnts,PatPick;
 private String Pat [] = { "ID : 1  |  Ali Fahad  | Age :  21  |  M  | 3:30PM 11/10/2025", "ID : 2  |  Sarah Mohammad  |  25  |  F  |  4:25PM  11/12/2025"};
 private String arrPatPick [] = { "Ali Fahad ", "Sarah Mohammad "};
-private JLabel LPatRec,PMeds,PName,LApptmnts;
+private JLabel LDocName,PMeds,PName,LApptmnts;
 private JTextField TFPMeds;
 
     public DoctorInterface(String title) {
     super(title);
-    this.setSize(300,300);
-    this.setLocation(100,400);
+    this.setLocation(200,400);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     JPanel MainP = (JPanel) this.getContentPane();
-    MainP.setLayout(new GridLayout(4,1));
+    MainP.setLayout(new BoxLayout (MainP,BoxLayout.Y_AXIS));
     
-    JPanel P1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel P1 = new JPanel(new GridLayout(2,1));
+    JPanel P11 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel P12 = new JPanel(new FlowLayout(FlowLayout.LEFT));
     
-    P1.setBorder(BorderFactory.createMatteBorder(0, 0, 5, 0, Color.GRAY));
-     LPatRec = new JLabel("Patient Records :");   
-     AccessBtn = new JButton("Access");
+     LDocName = new JLabel("Doctor : Nawaf Almutairi");
+     AccessBtn = new JButton("Access Patient Records");
      AccessBtn.addActionListener(new AccessActionListener());
-     P1.add(LPatRec);
-     P1.add(AccessBtn);
+     P11.add(LDocName);
+     P12.add(AccessBtn);
+     P1.add(P11);
+     P1.add(P12);
      //
      JPanel P2 = new JPanel(new BorderLayout());
      JPanel P21 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-     P2.setBorder(BorderFactory.createMatteBorder(0, 0, 5, 0, Color.GRAY));
+     JPanel P22 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+ 
      LApptmnts = new JLabel("Appointments: ");
      Apptmnts = new JComboBox(Pat);
      Can1Btn = new JButton("Cancel");
      ReschdBtn = new JButton("Reschedule");
      ReschdBtn.addActionListener(new ReschdActionListener());
-    P2.add(LApptmnts,BorderLayout.NORTH);
+     P2.add(LApptmnts,BorderLayout.NORTH);
      P21.add(Apptmnts );
-     P21.add(Can1Btn );
-     P21.add(ReschdBtn);
+     P22.add(Can1Btn );
+     P22.add(ReschdBtn);
      P2.add(P21,BorderLayout.CENTER);
+     P2.add(P22,BorderLayout.SOUTH);
      //
-     
      JPanel P3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-     JPanel P31 = new JPanel(new GridLayout(3,2));
-     JPanel P311 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-     GridBagConstraints a = new GridBagConstraints();
-     P3.setBorder(BorderFactory.createMatteBorder(0, 0, 5, 0, Color.GRAY));
      PMeds = new JLabel("Prescribe Medication : ");
-     PName = new JLabel("           Choose Patient :");
-     PatPick = new JComboBox(arrPatPick);
-     TFPMeds = new JTextField(30);
-     CnfrmBtn = new JButton("Confirm");
-     Can2Btn = new JButton("Cancel");
-     
-     P311.add(PMeds);
-     P311.add(PName);
-     P31.add(P311);
-     P31.add(PatPick);
-     P31.add(TFPMeds);
-     P31.add(new JLabel());
-     P31.add(CnfrmBtn);
-     P31.add(Can2Btn);
-     
-     P3.add(P31);
-            
+     AddMedBtn = new JButton("Add Perscribtion");
+     AddMedBtn.addActionListener(new AddMedActionListener());
+     P3.add(PMeds);
+     P3.add(AddMedBtn);       
     //
-    
     JPanel P4 = new JPanel();
     LogOutBtn = new JButton("Log Out");
     P4.add(LogOutBtn,BorderLayout.SOUTH);
-    
-    
     //
 
-     MainP.add(P1,a);
+     MainP.add(P1);
     
-     MainP.add(P2,a);
+     MainP.add(P2);
      
-     MainP.add(P3,a);
+     MainP.add(P3);
      
-     MainP.add(P4,a);
+     MainP.add(P4);
     
     
     
@@ -109,6 +97,15 @@ private JTextField TFPMeds;
         
     }
     
+     public class AddMedActionListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new PrescribtionInterface("Prescribe Medication");
+        }
+        
+    }
+    
     
     
 // 
@@ -128,6 +125,12 @@ private JTextField TFPMeds;
     CBDate = new JComboBox(arrDate);
     CnfrmBtn = new JButton("Confirm");
     RetBtn = new JButton("Return");
+    RetBtn.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        dispose();
+    }
+});
     P.add(CBDate,BorderLayout.NORTH);
     P1.add(CnfrmBtn);
     P1.add(RetBtn);
@@ -149,7 +152,7 @@ private JTextField TFPMeds;
 
         public PatientRecordsInterface(String title)  {
     super(title);
-    this.setSize(400,400);
+    this.setSize(400,200);
     this.setLocation(500,400);
     this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     JPanel P = (JPanel) this.getContentPane();
@@ -170,6 +173,51 @@ private JTextField TFPMeds;
         
         
         }
+       public class PrescribtionInterface extends JFrame {
+        private JLabel LMed ;
+        private JComboBox CBDate;
+        private String arrDate [] = { "Sat, 5:00PM 12/1/2025 ", "Mon, 5:30PM 12/3/2025"};
+        private JButton CnfrmBtn,RetBtn;
+
+        public PrescribtionInterface(String title)  {
+    super(title);
+    this.setSize(400,400);
+    this.setLocation(500,400);
+    this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    JPanel P = (JPanel) this.getContentPane();
+    P.setLayout(new GridLayout(3,1));
+    JPanel P1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel P2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+    JPanel P3 = new JPanel();
+     PName = new JLabel("Choose Patient :");
+     PatPick = new JComboBox(arrPatPick);
+     LMed = new JLabel("Medication : ");
+     TFPMeds = new JTextField(40);
+     CnfrmBtn = new JButton("Confirm");
+     Can2Btn = new JButton("Cancel");
+    Can2Btn.addActionListener(new ActionListener() {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        dispose();
+    }
+});
+
+     P1.add(PName);
+     P1.add(PatPick);
+     P2.add(LMed);
+     P2.add(TFPMeds);
+     P3.add(CnfrmBtn);
+     P3.add(Can2Btn);
+     P.add(P1);
+     P.add(P2);
+     P.add(P3);
+    
+    
+    this.pack();
+    this.show();
+            
+        }
+         }
    
     
     public static void main(String[] args) {
